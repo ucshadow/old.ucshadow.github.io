d67fa60ae9a64d6aaced621663828d06402afa4f
warleader <warleader@hotmail.co.uk>
2016-09-05 10:24:41 +0300

    test

--- raw/scripts/libs.js
+++ raw/scripts/libs.js
@@ -1,16 +1,16 @@
 export default function libUrl(lib) {
   // url's to framework icons
   var allUrls = {
-    Node: "http://i.imgur.com/kj7Vf48.png",
-    Meteor: "http://i.imgur.com/omS8IRf.png",
-    Bootstrap: "http://i.imgur.com/QoiOUgk.png",
-    Python: "http://i.imgur.com/amG0nzx.png",
-    "Material UI": "http://i.imgur.com/CyxO34A.png",
-    React: "http://i.imgur.com/7joJzVB.png",
-    jQuery: "http://i.imgur.com/ONbLw10.png",
-    mongoDB: "http://i.imgur.com/QYRfoqQ.png",
-    D3: "http://i.imgur.com/rnBzCfm.png",
-    Highcharts: "http://i.imgur.com/9R1IGev.png"
+    Node: "https://i.imgur.com/kj7Vf48.png",
+    Meteor: "https://i.imgur.com/omS8IRf.png",
+    Bootstrap: "https://i.imgur.com/QoiOUgk.png",
+    Python: "https://i.imgur.com/amG0nzx.png",
+    "Material UI": "https://i.imgur.com/CyxO34A.png",
+    React: "https://i.imgur.com/7joJzVB.png",
+    jQuery: "https://i.imgur.com/ONbLw10.png",
+    mongoDB: "https://i.imgur.com/QYRfoqQ.png",
+    D3: "https://i.imgur.com/rnBzCfm.png",
+    Highcharts: "https://i.imgur.com/9R1IGev.png"
   };
 
   return allUrls[lib];
